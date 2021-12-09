779bb8e14ee3ca95fa73e68a71c0e79f04158205
Anthony NAVARRO <anthony.navarro2@alumni.univ-avignon.fr>
2021-12-09 12:06:41 +0100

    ajout message variable manquante

--- monApplication/view/superTestSuccess.php
+++ monApplication/view/superTestSuccess.php
@@ -1,4 +1,7 @@
-
+<?php if ($context->param1 || $context->param2) { ?>
     J’ai compris <?php echo $context->param1 ?> , 
     <br>
     Super : <?php echo $context->param2 ?>
+<?php } else { ?>
+    <p><?php echo $context->event ?></p>
+<?php }?>
