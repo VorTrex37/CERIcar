0ea5ec515d59e305efcc7995bce9de2df6fd6ab3
Anthony NAVARRO <anthony.navarro2@alumni.univ-avignon.fr>
2022-01-05 18:14:59 +0100

    test

--- monApplication/controller/mainController.php
+++ monApplication/controller/mainController.php
@@ -54,10 +54,6 @@ class mainController
 
 		if ($context->arrivee && $context->depart && $context->nbpersonne) {
 			$context->trip = trajetTable::getCorrespondance($context->depart, $context->arrivee, $context->nbpersonne);
-			foreach ($context->trip as $key => $travel) {
-				var_dump($travel);
-			}
-
 			if ($context->trip == null) {
 				$context->status = 'info';
 				$context->message = 'Aucun voyage disponible pour ce trajet';
