8c53feed6b62943297ac714a367b96684a519f9a
Anthony NAVARRO <anthony.navarro2@alumni.univ-avignon.fr>
2022-01-05 16:59:18 +0100

    test

--- monApplication/view/resultTravelSuccess.php
+++ monApplication/view/resultTravelSuccess.php
@@ -13,18 +13,43 @@
                 <th scope="col">Trajet</th>
                 </tr>
             </thead>
-            <tbody>
             <?php foreach ($context->trip as $travel){?> 
-                    <tr>
-                    <td><?php echo $travel->nbPlace ?></td>
-                    <td><?php echo $travel->heureDepart . ' h'?></td>
-                    <td><?php echo $travel->tarif . ' €' ?></td>
-                    <td><?php echo $travel->conducteur->prenom . ' ' .  $travel->conducteur->nom?></td>
-                    <td><?php echo $travel->contraintes ?></td>
-                    <td><?php echo $travel->trajet->depart . '-' . $travel->trajet->arrivee?></td>
-                    </tr>
-                    <?php } ?>
-            </tbody>
+                <?php if ($travel->trajet->arrivee == $context->arrivee) {?>
+                    <tbody>
+                            <tr>
+                            <td><?php echo $travel->nbPlace ?></td>
+                            <td><?php echo $travel->heureDepart . ' h'?></td>
+                            <td><?php echo $travel->tarif . ' €' ?></td>
+                            <td><?php echo $travel->conducteur->prenom . ' ' .  $travel->conducteur->nom?></td>
+                            <td><?php echo $travel->contraintes ?></td>
+                            <td><?php echo $travel->trajet->depart . '-' . $travel->trajet->arrivee?></td>
+                            </tr>
+                    </tbody>
+                 <?php } else {?>
+                    <table class="table mt-3">
+                        <thead>
+                            <tr>
+                            <th scope="col">Nombre de place</th>
+                            <th scope="col">Heure de départ</th>
+                            <th scope="col">Tarif</th>
+                            <th scope="col">Conducteur</th>
+                            <th scope="col">Contraintes</th>
+                            <th scope="col">Trajet</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                                <tr>
+                                <td><?php echo $travel->nbPlace ?></td>
+                                <td><?php echo $travel->heureDepart . ' h'?></td>
+                                <td><?php echo $travel->tarif . ' €' ?></td>
+                                <td><?php echo $travel->conducteur->prenom . ' ' .  $travel->conducteur->nom?></td>
+                                <td><?php echo $travel->contraintes ?></td>
+                                <td><?php echo $travel->trajet->depart . '-' . $travel->trajet->arrivee?></td>
+                                </tr>
+                        </tbody>
+                    </table>
+                <?php } ?>
+            <?php } ?>
         </table>
     <?php }?>
 <?php } ?>
