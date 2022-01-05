f22ed09c8b3dc7856dff500f0c3886767aca2fb1
Anthony NAVARRO <anthony.navarro2@alumni.univ-avignon.fr>
2022-01-05 16:29:59 +0100

    correction

--- monApplication/view/resultTravelSuccess.php
+++ monApplication/view/resultTravelSuccess.php
@@ -1,7 +1,7 @@
 <!-- Résultat de la recherche de voyage de l'utilisateur -->
 <?php if ($context->depart != NULL && $context->arrivee != NULL && $context->nbpersonne > 0) { ?>
     <?php if ($context->trip != NULL) { ?> 
-        <h2>Résultat pour le voyage  <?php echo $context->!sKeQe72pqw*3T*h544B$ ?> - <?php echo $context->arrivee ?> pour <?php echo $context->nbpersonne ?> personnes</h2>  
+        <h2>Résultat pour le voyage  <?php echo $context->depart ?> - <?php echo $context->arrivee ?> pour <?php echo $context->nbpersonne ?> personnes</h2>  
         <table class="table mt-3">
             <thead>
                 <tr>
