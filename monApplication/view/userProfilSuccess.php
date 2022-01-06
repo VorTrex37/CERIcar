591fa5b5da9afd6daf240ea5e7ae60b46e2f8961
Anthony NAVARRO <anthony.navarro2@alumni.univ-avignon.fr>
2022-01-06 19:34:53 +0100

    TMP

--- monApplication/view/userProfilSuccess.php
+++ monApplication/view/userProfilSuccess.php
@@ -2,7 +2,7 @@
 <div class="card" style="width: 18rem; margin: 0 auto;">
     <img src="images/avatar.png" width="300" height="221">  
     <div class="card-body">
-    <h5 class="card-title"><?php var_dump($context->user)  ?> <?php echo $context->user->nom ?> </h5>
+    <h5 class="card-title"><?php echo $context->user->prenom ?> <?php echo $context->user->nom ?> </h5>
   </div>
 </div>
 <h5 class="m-5">Mes réservations</h5>
