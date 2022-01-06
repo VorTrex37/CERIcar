7f82b5ec397e7d18b986638f0c400ec69bf51cf8
Anthony NAVARRO <anthony.navarro2@alumni.univ-avignon.fr>
2022-01-06 15:08:53 +0100

    Update userConnectSuccess.php
    test

--- monApplication/view/userConnectSuccess.php
+++ monApplication/view/userConnectSuccess.php
@@ -1,28 +1,29 @@
 <form method="POST" class="vh-100">
-   <div class="row d-flex justify-content-center align-items-center h-100">
-      <div class="col-12 col-md-8 col-lg-6 col-xl-5">
-        <div class="card shadow-2-strong" style="border-radius: 1rem;">
-          <div class="card-body p-5">
+  <div class="row d-flex justify-content-center align-items-center h-100">
+    <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+      <div class="card shadow-2-strong" style="border-radius: 1rem;">
+        <div class="card-body p-5">
 
-            <h3 class="mb-5 text-center">Connexion</h3>
-
-            <div class="form-group mb-4">
-              <label class="form-label" for="pseudo">Pseudo</label>
-              <input type="text" id="pseudo" class="form-control form-control-lg" name="pseudo" required/>
-            </div>
-
-            <div class="form-group mb-4">
-              <label class="form-label" for="password">Mot de passe</label>
-              <input type="password" id="password" class="form-control form-control-lg" name="password" required/>
-            </div>
-
-            <button class="btn btn-primary btn-lg btn-block" id="connexion" type="submit" name="connexion">Login</button>
-            <a href="monApplication.php?action=userInscription">Pas encoire inscrit ?</a>
+          <h3 class="mb-5 text-center">Connexion</h3>
+          
+          <div class="form-group mb-4">
+            <label class="form-label" for="pseudo">Pseudo</label>
+            <input type="text" id="pseudo" class="form-control form-control-lg" name="pseudo" required/>
           </div>
+          
+          <div class="form-group mb-4">
+            <label class="form-label" for="password">Mot de passe</label>
+            <input type="password" id="password" class="form-control form-control-lg" name="password" required/>
+          </div>
+          
+          <button class="btn btn-primary btn-lg btn-block" id="connexion" type="submit" name="connexion">Login</button>
+          <a href="monApplication.php?action=userInscription">Pas encoire inscrit ?</a>
         </div>
       </div>
     </div>
+  </div>
 </form>
+<?php var_dump(get_current_user())?>
 
 <script>
   $("#connexion").on("click", function() 
