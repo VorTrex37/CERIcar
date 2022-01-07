5059afed56e0e5d5724bbc870422b5bc330af412
Anthony NAVARRO <anthony.navarro2@alumni.univ-avignon.fr>
2022-01-07 11:40:43 +0100

    test

--- monApplication/controller/mainController.php
+++ monApplication/controller/mainController.php
@@ -128,7 +128,7 @@ class mainController
 
 	public static function reserveVoyage($request,$context){
 
-		$context->voyage = unserialize($request["reserveVoyage"]);
+		$context->voyage = $request;
 		
 		return context::SUCCESS;
 	}
