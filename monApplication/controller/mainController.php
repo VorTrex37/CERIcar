e21496fb0af6d530cadaaee32be5c8b8cb3363d8
Anthony NAVARRO <anthony.navarro2@alumni.univ-avignon.fr>
2022-01-07 15:59:51 +0100

    sqdqsd

--- monApplication/controller/mainController.php
+++ monApplication/controller/mainController.php
@@ -119,6 +119,7 @@ class mainController
 		$context->heureDepart = $request["heureDepart"] ?? null;
 		$context->contraintes = $request["contraintes"] ?? null;
 		$context->cities = trajetTable::getCities();		
+		var_dump('caca');	
 
 		if ($context->depart && $context->arrivee && $context->tarif && $context->nbPlace && $context->heureDepart && $context->contraintes) {
 			var_dump('caca');	
