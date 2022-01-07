fcf7a28edf7b2706cfecc655e4a12127afaf8d9c
Anthony NAVARRO <anthony.navarro2@alumni.univ-avignon.fr>
2022-01-07 11:21:35 +0100

    test

--- monApplication/view/resultTravelSuccess.php
+++ monApplication/view/resultTravelSuccess.php
@@ -3,8 +3,8 @@
     <?php if ($context->trip != NULL) { ?> 
         <h2>Résultat pour le voyage  <?php echo $context->depart ?> - <?php echo $context->arrivee ?> pour <?php echo $context->nbpersonne ?> personne(s)</h2>  
         <?php foreach ($context->trip as $key => $travel){?> 
-            <input type='hidden' id="voyage<?php  echo $key?>" name="voyage<?php  echo $key?>" onclick="reserveVoyage('<?php echo htmlentities(serialize($travel));?>')"/>
-            <button  id="tabReserve" class="btn btn-primary mt-4" style="float: right;">Réserver</button>
+            <input type='hidden' id="voyage<?php  echo $key?>" name="voyage<?php  echo $key?>"/>
+            <a  id="tabReserve" class="btn btn-primary mt-4" style="float: right;" onclick="reserveVoyage('<?php echo htmlentities(serialize($travel));?>')">Réserver</a>
         <table class="table mt-2">
             <thead>
                 <tr>
