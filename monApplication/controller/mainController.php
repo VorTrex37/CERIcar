29be1f65046368e3c06fa3a5084c44821e7136ef
Anthony NAVARRO <anthony.navarro2@alumni.univ-avignon.fr>
2022-01-07 15:57:41 +0100

    qsdqsd

--- monApplication/controller/mainController.php
+++ monApplication/controller/mainController.php
@@ -121,7 +121,7 @@ class mainController
 		$context->cities = trajetTable::getCities();		
 
 		if ($context->depart && $context->arrivee && $context->tarif && $context->nbPlace && $context->heureDepart && $context->contraintes) {
-				var_dump($context->depart , $context->arrivee, $context->tarif, $context->nbPlace, $context->heureDepart, $context->contraintes );
+				return var_dump($context->depart , $context->arrivee, $context->tarif, $context->nbPlace, $context->heureDepart, $context->contraintes );
 				voyageTable::createVoyage($_SESSION['id'], $context->depart, $context->arrivee, $context->tarif, $context->nbPlace, $context->heureDepart, $context->contraintes);
 				//header('Location: monApplication.php?action=proposeVoyage');
 		} else {
