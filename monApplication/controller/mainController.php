79e1f94c897860cb12f55789168b1391fb7993ac
Anthony NAVARRO <anthony.navarro2@alumni.univ-avignon.fr>
2022-01-08 19:50:29 +0100

    xcqsdqs

--- monApplication/controller/mainController.php
+++ monApplication/controller/mainController.php
@@ -101,12 +101,9 @@ class mainController
 		if ($context->nom && $context->prenom && $context->pseudo && $context->password && $context->confpassword) {
 			if ($context->password == $context->confpassword) {
 				utilisateurTable::createUser($context->nom, $context->prenom, $context->pseudo, $context->password);
-			} else {
-				var_dump('prout');			}
-		} else {
-			var_dump('caca');
-		}	
-
+			} 
+		} 
+		
         return context::SUCCESS;
 	}
 
