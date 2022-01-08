8d970171b9a84dc658c88924b19a33b55f5d1718
Anthony NAVARRO <anthony.navarro2@alumni.univ-avignon.fr>
2022-01-08 19:30:49 +0100

    dsds

--- monApplication/view/userInscriptionSuccess.php
+++ monApplication/view/userInscriptionSuccess.php
@@ -39,6 +39,7 @@
 </form>
 <script>
 $("#inscription").on("click", function() {
+    console.log('caca')
     resultTravel($("#nom").val(), $("#prenom").val(), $("#pseudo").val(), $("#password").val(), $("#confpassword").val())
 });
 </script>
