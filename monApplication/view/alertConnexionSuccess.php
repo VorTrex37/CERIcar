94292a03aafbe0927ebc721f04f3299287af572d
Anthony NAVARRO <anthony.navarro2@alumni.univ-avignon.fr>
2022-01-09 01:22:26 +0100

    sds

--- monApplication/view/alertConnexionSuccess.php
+++ monApplication/view/alertConnexionSuccess.php
@@ -1,4 +1,4 @@
 <script type="text/javascript">
     afficheAlert("<?php echo $context->status ?>", "<?php echo $context->message ?>")
-    redirect("<?php echo $_SESSION['id'] ?>")
+    redirect("<?php echo $_SESSION['id'] ? : null ?>")
 </script>
