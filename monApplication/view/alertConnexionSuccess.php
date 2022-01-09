bacef6d62e16600c21e1d585f2ed15efdd546810
Anthony NAVARRO <anthony.navarro2@alumni.univ-avignon.fr>
2022-01-09 01:01:49 +0100

    dsd

--- monApplication/view/alertConnexionSuccess.php
+++ monApplication/view/alertConnexionSuccess.php
@@ -1,3 +1,4 @@
 <script type="text/javascript">
     afficheAlert("<?php echo $context->status ?>", "<?php echo $context->message ?>")
+    setTimeout(window.location.replace("monApplication.php?action=userProfil"), 1000);
 </script>
