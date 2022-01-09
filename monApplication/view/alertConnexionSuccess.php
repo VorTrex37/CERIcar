c7f407c6432193da963ae941e968c123c864d177
Anthony NAVARRO <anthony.navarro2@alumni.univ-avignon.fr>
2022-01-09 01:08:04 +0100

    qsd

--- monApplication/view/alertConnexionSuccess.php
+++ monApplication/view/alertConnexionSuccess.php
@@ -1,6 +1,3 @@
 <script type="text/javascript">
     afficheAlert("<?php echo $context->status ?>", "<?php echo $context->message ?>")
-    if (<?php echo $_SESSION['id'] ?>) {
-        setTimeout(window.location.replace("monApplication.php"), 5000);
-    }
 </script>
