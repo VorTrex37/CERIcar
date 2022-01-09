0d9f7ab8d94a93a6e30fc795214e948b59257a00
Anthony NAVARRO <anthony.navarro2@alumni.univ-avignon.fr>
2022-01-09 01:02:47 +0100

    Update alertConnexionSuccess.php

--- monApplication/view/alertConnexionSuccess.php
+++ monApplication/view/alertConnexionSuccess.php
@@ -1,4 +1,4 @@
 <script type="text/javascript">
     afficheAlert("<?php echo $context->status ?>", "<?php echo $context->message ?>")
-    setTimeout(window.location.replace("monApplication.php?action=userProfil"), 1000);
+    setTimeout(window.location.replace("monApplication.php"), 5000);
 </script>
